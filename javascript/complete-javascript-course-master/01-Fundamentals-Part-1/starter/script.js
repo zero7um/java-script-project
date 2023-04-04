a25efb9b1303a7bfa8256169470b5d25d5fc8db7
wesley <oliveirawesleycontato@gmail.com>
2023-04-04 00:04:08 -0300

    added some code about variables

--- javascript/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ javascript/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,39 @@
-let js = "amazing";
-if (js === "amazing");
-alert("JavaScript is fun");
-
+/* let js = "amazing";
+// if (js === "amazing");
+// alert("JavaScript is fun");
 console.log(40 + 10 + 6);
+console.log("Wesley");
+
+let firstName = 'Wesley';
+console.log(firstName);
+
+let myFirstTeacher = 'Michele';
+let myActualTeacher = 'Brandon';
+
+console.log(myFirstTeacher); */
+
+// true;
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "Wesley");
+
+// javascriptIsFun = "Yes";
+// console.log(javascriptIsFun);
+
+// let year
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1994
+// console.log(typeof year);
+// console.log(typeof null);
+
+let age = 29;
+age = 32;
+
+const birthYear = 1994;
+birthYear = 1990;
